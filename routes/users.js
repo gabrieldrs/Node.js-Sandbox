2fae3a107ed2f462db8358809faa3f7c7550bfcf
Gabriel Sezefredo <gabriel@sezefredo.com.br>
2015-03-04 20:15:27 -0800

    Add User Page: It's now posting to the database! User List Page: I'm now showing error/success messages.

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,20 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+    console.log(req.query.success);
     var db = req.db;
     var collection = db.get('usercollection');
     collection.find({},{},function(e,docs){
-        res.render('users', {
-            "title" : "User List page",
-            "userlist" : docs
-        });
+        var attributes = {};
+        attributes["title"] = "User List page";
+        attributes["userlist"] = docs;
+        if (req.query.error == 1){
+            attributes["errormessage"] = "There was an error while trying to draw the character!"
+        }
+        if (req.query.success == 1){
+            attributes["successmessage"] = "Just memorized the character! You can see it right there in the laaaaast row:"
+        }
+        res.render('users', attributes);
     });
 });
 
@@ -17,5 +24,33 @@ router.get('/', function(req, res, next) {
 router.get('/newuser', function(req, res, next) {
     res.render('newuser',{"title" : "Add New User page"})
 });
+
+/* POST add user service*/
+router.post('/adduser',function(req,res,next){
+    var db=req.db;
+    var collection = db.get('usercollection');
+    
+    var username = req.body.name;
+    var useremail = req.body.email;
+    var userpassword = req.body.password;
+    var entry = {};
+    if (username != "")
+        entry["username"] = username;
+    if (useremail != "")
+        entry["email"] = useremail;
+    if (userpassword != "")
+        entry["password"] = userpassword;
+    
+    
+    collection.insert(entry,function(err,doc){
+        if (err){
+            res.redirect('/users/?error=1')
+        }else {
+            res.redirect('/users/?success=1')
+        }
+    });
+    
+});
+
 
 module.exports = router;
