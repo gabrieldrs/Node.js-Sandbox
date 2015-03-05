b1fde041ba0d3d31a94435ca6a80dcd1314d5297
Gabriel Sezefredo <gabriel@sezefredo.com.br>
2015-03-04 19:11:05 -0800

    User List Page: Created the User List Page; Database Parsing: Parsed data stored in my database as an attribute for the jade file using routes.

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('users', {
+            "title" : "User List page",
+            "userlist" : docs
+        });
+    });
 });
 
 module.exports = router;
