e3dbe971ba209acc57ee0e5e1f376a20db566f40
Gabriel Sezefredo <gabriel@sezefredo.com.br>
2015-03-04 18:41:59 -0800

    Hello World Page: Created the Hello World Page; Index Page: Created a link to the newly created Hello World Page; DataBase: Started a MongoDB database.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/helloworld', function(req, res, next) {
+    res.render('hw', { title: 'Hey World, how are ya!?' });
 });
 
 module.exports = router;
